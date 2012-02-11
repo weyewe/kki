993c3cdd1bcc80e03c126af6096a200f33c6edfd
weyewe <w.yunnal@gmail.com>
2012-02-11 23:52:35 +0700

    added seeds for role, and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,84 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-super_user_role     = Role.create :name => "SuperUser"
-admin_role          = Role.create :name => "Admin"
-cashier_role        = Role.create :name => "Cashier"
-loan_creator_role   = Role.create :name => "LoanCreator"
-debt_collector_role = Role.create :name => "DebtCollector"
+=begin
+Role creation
+What are the roles in typical branch? 
+1. Branch Manager
+  -> Approve Group Creation
+  -> Approve GroupLoan Disbursement 
+  -> Approve Member Creation
+  -> Closing Group Loan 
+    - approve that everyone has paid
+  -> Create Loan Product 
+  -> Updating, cancelling all these informations 
+  
+  -> Add employee
+    -> add role 
+  -> Add greographical work scope (Kecamatan)
+  
+  
+  Branch manager only acts as controller. Making sure that everyone is doing the right thing,
+  and making the critical decision
+  
+2. Loan Officer
+  -> Registering approved member
+  -> Create group    
+    -> Assign member to a group -> Pick a group leader 
+    
+  -> Create subgroup    
+    -> Assign group member to the subgroup   -> Pick the subgroup leader
+  
+    
+  
+3. Field Worker 
+  -> Add Payment
+    Group Creation Payment:
+    -> Admin Fee 
+    -> Initial Savings
+    
+    Weekly Payment:
+    -> Savings
+    -> Principal
+  
+  -> Add Weekly Meeting 
+    -> Meeting attendance
+    
+4. Cashier 
+  -> Approve weekly payment 
+  -> Unapprove weekly payment 
+    -> resolve with the responsible field worker 
+=end
 
-# when a new company is created, it is logged with role SuperUser, granted with Admin rights
+Role.all.each { |role| role.destroy }
+
+branch_manager_role = Role.create :name => "BranchManager"
+loan_officer_role   = Role.create :name => "LoanOfficer"
+field_worker_role   = Role.create :name => "FieldWorker"
+cashier_role         = Role.create :name => "Cashier"
+
+=begin
+All these roles are enough to manage branch office
+=end
+
+Assignment.all.each { |x| x.destroy }
+User.all.each { |x| x.destroy }
+
+branch_manager = User.create :email => "branch_manager@gmail.com", :password => "branch1234",
+                  :password_confirmation => "branch1234"
+branch_manager.roles << branch_manager_role
+branch_manager.save
+                  
+loan_officer = User.create :email => "loan_officer@gmail.com", :password => "loan1234",
+                  :password_confirmation => "loan1234"
+loan_officer.roles << loan_officer_role
+loan_officer.save      
+            
+                  
+field_worker = User.create :email => "field_worker@gmail.com", :password => "field1234",
+                  :password_confirmation => "field1234"
+field_worker.roles << field_worker_role
+field_worker.save                
+
+cashier = User.create :email => "cashier@gmail.com", :password => "cashier1234",
+                  :password_confirmation => "cashier1234"
+cashier.roles << cashier_role
+cashier.save
