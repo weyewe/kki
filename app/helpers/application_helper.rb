29e286c50c13b2a554035eaf6de13ad33bb55ea7
weyewe <w.yunnal@gmail.com>
2012-09-27 17:42:34 +0700

    update print money

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -81,7 +81,12 @@ module ApplicationHelper
 =end
 
   def print_money(value)
-    number_with_delimiter( value.to_i , :delimiter => "." )
+    if value.nil?
+      number_with_delimiter( 0 , :delimiter => "." )
+    else
+      number_with_delimiter( value.to_i , :delimiter => "." )
+    end
+    
   end
   
   
